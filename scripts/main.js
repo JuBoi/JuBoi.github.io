2062ad30aa45c32dbdcc555da5689ae8f721611f
Bryan Ju <jubryanju@gmail.com>
2019-06-14 20:13:39 -0500

    This works

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,27 @@
 var pokeball = document.getElementById('pokeball');
 
 var heading = document.getElementById('heading');
-var i = 0;
 
-function smaller() {
-	pokeball.style.width -= 20;
-	pokeball.style.height -= 20;
-}
+var i = 0;
+var size = 300;
 
 pokeball.onclick = function() {
 	if(i == 0) {
+		size -= 20;
 		heading.innerHTML = "CLICK ME";
-		smaller();
+		pokeball.style.width = String(size) + "px";
+		pokeball.style.height = String(size) + "px";
 	}
 	else {
+		size -= 20;
 		heading.innerHTML = heading.innerHTML + " MORE";
-		smaller();
+		pokeball.style.width = String(size) + "px";
+		pokeball.style.height = String(size) + "px";
 	}
 	if(i > 9) {
 		pokeball.src = "images/halle.jpg"
+		pokeball.style.width = "400px";
+		pokeball.style.height = "400px";
 		heading.innerHTML = "the best player"
 	}
 	i++;
