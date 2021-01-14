0cf907cd24ed57c7ec0cc2e4da6bd9a7d87f12d2
Ju <bryanju2@illinois.edu>
2021-01-14 00:40:54 -0600

    hi

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,28 @@
-var pokeball = document.getElementById('pokeball');
+var pietro = document.getElementById('pietro');
 
 var heading = document.getElementById('heading');
 
 var i = 0;
 var size = 300;
 
-pokeball.onclick = function() {
+pietro.onclick = function() {
 	if(i == 0) {
 		size -= 20;
 		heading.innerHTML = "CLICK ME";
-		pokeball.style.width = String(size) + "px";
-		pokeball.style.height = String(size) + "px";
+		pietro.style.width = String(size) + "px";
+		pietro.style.height = String(size) + "px";
 	}
 	else {
 		size -= 20;
 		heading.innerHTML = heading.innerHTML + " MORE";
-		pokeball.style.width = String(size) + "px";
-		pokeball.style.height = String(size) + "px";
+		pietro.style.width = String(size) + "px";
+		pietro.style.height = String(size) + "px";
 	}
 	if(i > 9) {
-		pokeball.src = "images/peterpose.jpg"
-		pokeball.style.width = "400px";
-		pokeball.style.height = "800px";
-		heading.innerHTML = "THE Peter Pose"
+		pietro.src = "images/valentine.jpg"
+		pietro.style.width = "400px";
+		pietro.style.height = "400px";
+		heading.innerHTML = "hey sweetcheeks"
 	}
 	i++;
 }
